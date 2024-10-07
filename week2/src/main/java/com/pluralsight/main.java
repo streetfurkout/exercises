cf90b49fc3ea9d53590747652653c7bcfd62dd34
Furkan <furkanamerikaa@gmail.com>
2024-10-06 20:59:08 -0400

    week2

---
+++ week2/src/main/java/com/pluralsight/main.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+/*        String fullName = "Furkan";
+        fullName = fullName.trim();
+        fullName = fullName.toUpperCase();
+        fullName = fullName.toLowerCase();
+
+        int length = fullName.length();
+
+        if (fullName.startsWith("F")) ;
+        {
+            System.out.println("This code is true");
+        }
+        if (fullName.endsWith("n")) ;
+        {
+            System.out.println("This code is true");
+        }
+charAt and indefOf() these are important
+    }
+*/
+
+       
+
+    }
+}
+
